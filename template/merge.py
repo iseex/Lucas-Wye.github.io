729455fa6ee3ce4f2b50d8749101798e6b6642ce
Zara-Wye <229559117@qq.com>
2019-10-05 08:59:16 +0800

    update new passage

--- template/merge.py
+++ template/merge.py
@@ -4,6 +4,20 @@
 '''
 import os, re, codecs
 
+def merge_html(filePath, filelist):
+  for file in filelist:
+    if 'html' in file:
+      print(file)
+      # '''
+      with open(filePath+"/"+file,'r') as f:
+        tmp = f.read()
+      txtAll = txtFront + tmp + txtBack
+
+      # 输出
+      with open('../' + file, 'w') as f:# 后打印
+        f.write(txtAll)
+      # '''
+
 
 # 构建header
 with open('./fixed/header.html', 'r') as f:
@@ -13,21 +27,10 @@ with open('./fixed/header.html', 'r') as f:
 with open('./fixed/footer.html', 'r') as f:
   txtBack = f.read()
 
-  
-filePath = '.'
-filelist = os.listdir(filePath)
-for file in filelist:
-  if 'html' in file:
-    print(file)
-    # '''
-    with open('./'+file,'r') as f:
-      tmp = f.read()
-    txtAll = txtFront + tmp + txtBack
-
-    # 输出
-    with open('../' + file, 'w') as f:# 后打印
-      f.write(txtAll)
-    # '''
+filePathlist = ['.', './note','./post']
+for filePath in filePathlist:
+  filelist = os.listdir(filePath)
+  merge_html(filePath,filelist)
 
 
 # 结束
